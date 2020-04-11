f1137b39ed2bf0699a19759cd84e953a1cd7ac86
vandermnt <vanderson.mantovani@hotmail.com>
2020-04-10 23:50:15 -0300

    front-end

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [project, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories',{
+      	title: `Novo projeto ${Date.now()}`,
+	      url: "www.google.com",
+	      techs: "Tecnologias Testando",
+	      likes: 0
+    });
+
+    const projects = response.data;
+
+    setProjects([...project, projects]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    
+    setProjects(project.filter(projects => projects.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {project.map(project => 
+        <li key={project.id}>  
+          {project.title} 
+          
+          <button onClick={() => handleRemoveRepository(project.id)}>  
             Remover
           </button>
-        </li>
+        
+        </li>)}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
